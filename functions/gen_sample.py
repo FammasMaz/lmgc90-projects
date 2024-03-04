[feature, fix] closet: add more options for scripts and fix computation
Signed-off-by: Moeeze Hassan <fammas.maz@gmail.com>

functions/gen_sample.py
@@ -618,20 +618,21 @@ def closet_ballast(par_dir, seed=687, visu=False, step=1, args=None):
     dim=3
     # box dimensions
     thick = 0.01
-    lengthb = 1.
-    widthb = 1.
+    lengthb = 1.435
+    widthb = 0.8
     heightb = 1.
 
     # ballast params
     ballast_bib = 'BIBLIGRAINS/BIBLIGRAINS.DAT'
     dgrid = 0.1
-    nbx = int(lengthb/dgrid)
-    # minus the width decreased by slanted plates
-    nby = int(widthb/dgrid) - int(nbx*np.sin(np.deg2rad(20))) -1
-    nlayer = int(args.n_layers*1.8/0.4)
-
-
-
+    angle_for_plate = args.angle_for_plate
+    nbx = int(lengthb/dgrid) -1
+    # minus the width decreased by slanted plates - 
+    nby = int(widthb/dgrid  )# override
+    if angle_for_plate != 0:
+        # minus the width decreased by slanted plates -
+        nby =nby - int((lengthb*np.tan(np.deg2rad(angle_for_plate)))/dgrid) -1 
+    nlayer = int(args.n_layers)
     
     # material dict and container
     dict_mat = {'TDURx': {'materialType':'RIGID', 'density':2700.}}
@@ -646,7 +647,7 @@ def closet_ballast(par_dir, seed=687, visu=False, step=1, args=None):
     bodies = pre.avatars()
     if args.ballast:
 
-        bodies, z = ballast_generator_closet(ballast_bib, nbx, nby, nlayer, dgrid, lengthb, widthb, mats['TDURx'], mods['rigid'])
+        bodies, z = ballast_generator_closet(ballast_bib, nbx, nby, nlayer, dgrid, lengthb, widthb, mats['TDURx'], mods['rigid'], angle_for_plate=angle_for_plate)
     
     if args.closet:
         dict = {
@@ -656,8 +657,8 @@ def closet_ballast(par_dir, seed=687, visu=False, step=1, args=None):
             'axe3': thick/2,
             'x': 0.,
             'y': -widthb/2 - thick/2,
-            'z': heightb/2,
-            'rotate_Ax':{ 'theta': np.deg2rad(-10)},
+            'z': heightb/2.,
+            'rotate_Ax':{ 'theta': np.deg2rad(-angle_for_plate)},
             'rotate':{'theta': -0.5*np.pi},
             'imposeDrivenDof': {'component':[1,2,3,4,5,6]},
             'DrivenDof':{'vx': -2,'name':'vleft','component':2,'path':par_dir}, 
@@ -668,8 +669,8 @@ def closet_ballast(par_dir, seed=687, visu=False, step=1, args=None):
             'axe3': thick/2,
             'x': 0.,
             'y': widthb/2 + thick/2,
-            'z': heightb/2,
-            'rotate_Ax':{ 'theta': np.deg2rad(10)},
+            'z': heightb/2.,
+            'rotate_Ax':{ 'theta': np.deg2rad(angle_for_plate)},
             'rotate':{'theta': 0.5*np.pi},
             'imposeDrivenDof': {'component':[1,2,3,4,5,6]},
             'DrivenDof':{'vx': 2,'name':'vright','component':2,'path':par_dir}, 
@@ -681,9 +682,11 @@ def closet_ballast(par_dir, seed=687, visu=False, step=1, args=None):
             'axe3': thick/2,
             'x': lengthb/2 + thick/2,
             'y': 0.,
-            'z': heightb/2,
+            'z': heightb/2.,
             'rotate':{'description':'axis', 'alpha': -0.5*np.pi, 'axis':[0.,1.,0.]},
-            'imposeDrivenDof': {'component':[1,2,3,4,5,6]}
+            'imposeDrivenDof': {'component':[1,2,3,4,5,6]},
+            'DrivenDof':{'vx': 2,'name':'vfront','component':1,'path':par_dir}, 
+
         },
         'back':{
             'axe1': heightb/2 ,
@@ -691,9 +694,10 @@ def closet_ballast(par_dir, seed=687, visu=False, step=1, args=None):
             'axe3': thick/2,
             'x': -lengthb/2 - thick/2,
             'y': 0.,
-            'z': heightb/2,
+            'z': heightb/2.,
             'rotate':{'description':'axis', 'alpha': 0.5*np.pi, 'axis':[0.,1.,0.]},
-            'imposeDrivenDof': {'component':[1,2,3,4,5,6]}
+            'imposeDrivenDof': {'component':[1,2,3,4,5,6]},
+            'DrivenDof':{'vx': -2,'name':'vback','component':1,'path':par_dir}, 
         },
         'bottom':{
             'axe1': 2+0.1,
@@ -712,12 +716,12 @@ def closet_ballast(par_dir, seed=687, visu=False, step=1, args=None):
             'y': 0.,
             'z': max(z)+dgrid/2.,
             'rotate':{'theta': np.pi},
-            'imposeDrivenDof': {'component':[1,2,4,5,6]},
-            'DrivenDof':{'vx': 3,'name':'vtop','component':3,'path':par_dir, 'vinit': -9.8}, 
-
+            'imposeDrivenDof': {'component':[1,2,3,4,5,6]},
+            #'DrivenDof':{'vx': 3,'name':'vtopleft','component':2,'path':par_dir, 'vinit': -9.8, 'start': 0.}, 
+            'pullup':{'acc': -9.8, 'mass': 2700 * lengthb*widthb *thick, 'path':par_dir,'component':3, 'name':'ftop'},
         }
         }
-    bodies_plates = plate_definition(dict, mat=mats['TDURx'], mod=mods['rigid'])
+    bodies_plates = plate_definition(dict, mat=mats['TDURx'], mod=mods['rigid'],dt=args.dt, time=args.time)
     
     bodies+=bodies_plates
     if args.visu:pre.visuAvatars(bodies)
@@ -731,22 +735,18 @@ def closet_ballast(par_dir, seed=687, visu=False, step=1, args=None):
     # see dict and container
     dict_pp = {'vpp': {'CorpsCandidat':'RBDY3', 'candidat':'POLYR','colorCandidat':'BLUEx','behav':tacts['iqsc1'],
                         'CorpsAntagoniste':'RBDY3', 'antagoniste':'POLYR','colorAntagoniste':'BLUEx',
-                        'alert':0.001}}
+                        'alert':1.4044198827808083E-003}}
 
     dict_pw = {'vpw': {'CorpsCandidat':'RBDY3', 'candidat':'POLYR','colorCandidat':'BLUEx','behav':tacts['iqsc1'],
                         'CorpsAntagoniste':'RBDY3', 'antagoniste':'PLANx','colorAntagoniste':'VERTx',
-                        'alert':0.001}}
-    
+                        'alert':1.4044198827808083E-003}}
+
     dict_see = {}
     if args.ballast: dict_see.update(dict_pp)
     if args.closet: dict_see.update(dict_pw)
 
     svs = create_see_tables(dict_see)
-    post_dict = {'CONTACT FORCE DISTRIBUTION': {'step':step, 'val':1},
-                'COORDINATION NUMBER': {'step':step},
-                'AVERAGE VELOCITY EVOLUTION': {'step':step, 'color':'BLUEx'},
-                'KINETIC ENERGY': {'step':step},
-                'DISSIPATED ENERGY': {'step':step}
+    post_dict = {'CONTACT FORCE DISTRIBUTION': {'step':step, 'val':1}
                 }
     post = create_postpro_commands(post_dict)
     pre.writeDatbox(dim,mats,mods,bodies,tacts,svs,post=post, datbox_path=os.path.join(par_dir,'DATBOX'))

utilities/generators.py
@@ -110,7 +110,7 @@ def ballast_generator_custom(ballast_bib, layers, nb_particles, Px, Py, Pz, mat,
   return bodies, total_particles, particle_char
 
 
-def ballast_generator_closet(ballast_bib, nbx, nby, nlayer, dgrid, lengthb, widthb, mat, model, color='BLUEx'):
+def ballast_generator_closet(ballast_bib, nbx, nby, nlayer, dgrid, lengthb, widthb, mat, model, color='BLUEx', angle_for_plate=0.):
     bodies = pre.avatars()
     lballast = get_lbody(ballast_bib)
     coor = pre.cubicLattice3D(nbx, nby, nlayer, dgrid, x0=-lengthb/2, y0=-widthb/2, z0=0.01)
@@ -124,12 +124,12 @@ def ballast_generator_closet(ballast_bib, nbx, nby, nlayer, dgrid, lengthb, widt
         body = pre.rigidPolyhedron(model = model, material=mat, color=color, generation_type='full', vertices=lballast[rand][0], faces=lballast[rand][1])
         r = np.random.random(3)*np.pi
         body.rotate(phi=r[0],theta=r[1],psi=r[2])
-        body.translate(dx=x[i], dy=y[i]+ 0.15, dz=z[i])
+        body.translate(dx=x[i]+0.1, dy=y[i]+0.1 + np.tan(np.deg2rad(angle_for_plate)/2), dz=z[i])
         i+=1
         bodies.addAvatar(body)
     return bodies, z
 
-def plate_definition(dict, mod, mat):
+def plate_definition(dict, mod, mat, dt, time):
    bodies = pre.avatars()
    for k, v in dict.items():
       # the key is the name of the plate
@@ -142,8 +142,8 @@ def plate_definition(dict, mod, mat):
       plate.defineModel(model=mod)
       plate.defineMaterial(material=mat)
       # define contactors with respect to dic value
-
-      plate.addContactors('PLANx', color='VERTx', axe1=v['axe1'], axe2=v['axe2'], axe3=v['axe3'], group='all')
+      color = 'VERTx' if 'color' not in v else v['color']
+      plate.addContactors('PLANx', color=color, axe1=v['axe1'], axe2=v['axe2'], axe3=v['axe3'], group='all')
       plate.computeRigidProperties()
       if 'rotate_Ax' in v:
          plate.rotate(center=plate.nodes[1].coor,**v['rotate_Ax'])
@@ -153,7 +153,7 @@ def plate_definition(dict, mod, mat):
       plate.imposeDrivenDof(dofty='vlocy',**v['imposeDrivenDof'])
       if 'DrivenDof' in v:
         def imposedv(t):
-          if t > 4.0:
+          if t > (time-0.5):
             return vx
           else:
             # return gravity
@@ -164,9 +164,19 @@ def plate_definition(dict, mod, mat):
         fname = v['DrivenDof']['name'] + '.dat'
         if 'start' not in v['DrivenDof']:start = 0.
         else: start = v['DrivenDof']['start']
-        pre.writeEvolution(f=imposedv, instants=np.linspace(start,4.5, 9000), path=v['DrivenDof']['path'] + 'DATBOX/',name=fname)
+        pre.writeEvolution(f=imposedv, instants=np.linspace(start,time, int(1/dt)), path=v['DrivenDof']['path'] + 'DATBOX/',name=fname)
         plate.imposeDrivenDof(component=v['DrivenDof']['component'], description='evolution',dofty='vlocy', evolutionFile=fname)
-      
+
+      if 'pullup' in v:
+         # previously the body fell under gravity now pull it up
+         def drivenForce(t):
+            if t > (time-0.5): 
+               return 1800
+            else:
+               return -4.9
+            
+         pre.writeEvolution(f=drivenForce, instants=np.linspace(0,time,int(1/dt)), path=v['pullup']['path'] + 'DATBOX/',name=v['pullup']['name'] + '.dat')
+         plate.imposeDrivenDof(component=v['pullup']['component'], description='evolution',dofty='force', evolutionFile=v['pullup']['name'] + '.dat')
       bodies.addAvatar(plate)
    return bodies
 